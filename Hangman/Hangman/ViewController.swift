6f752104c2917854f7cc9ca7494baef18296bdf3
ibraheemraw <ibraheemrawlinson@pursuit.org>
2018-11-27 17:51:17 -0500

    evaluated both text fields

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var strikesImages: UIImageView!
     @IBOutlet weak var player1UserInput: UITextField!
-    @IBOutlet weak var player2UserInput: UITextField! 
+    @IBOutlet weak var player2UserInput: UITextField!
     @IBOutlet weak var hangmanWord: UILabel!
+    var imageArr: [UIImage]!
+    
     override func viewDidLoad() {
     super.viewDidLoad()
         player1UserInput.delegate = self
         player2UserInput.delegate = self
+        
+       
     
   }
 
@@ -28,20 +32,23 @@ extension ViewController: UITextFieldDelegate {
     // step 2: implement methods as needed
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // dismiss keyboard
+        print("Player1 entered \(player1UserInput.text ?? "nothing entered")")
         textField.resignFirstResponder()
+        player1UserInput.isEnabled = false
+        
+        if textField.text == player2UserInput.text {
+            hangmanWord.text = "Correct!! The Word was:2 \(player1UserInput.text ?? "Error With Code")"
+        } else {
+            strikesImages.image = UIImage(named: "hang1")
+            print("Sorry you entered: \(player2UserInput.text ?? "default value")")}
         
         
-        //        if textField == player2LetterInput {
-        //            // do cohort logic
-        //            print("Correct")
-        //        }
-        
-        print("user entered \(player1UserInput.text ?? "nothing entered")")
-        
+        print("Player2 entered \(player2UserInput.text ?? "nothing entered")")
         // does not matter here is true or false
         // since textfield behaviour is on one line
         return true
     }
+   
     func textField(_ textField: UITextField,
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool {
@@ -54,14 +61,21 @@ extension ViewController: UITextFieldDelegate {
         case player2UserInput:
             guard let text = textField.text else { return true }
             let count = text.count + string.count - range.length
-            return count <= typingRange
+//             checkForHangman(player2UserInput, player1UserInput)
+            return count <= 36
         default:
             print("Error")
             return true
         }
-        
-        
     }
     
+    func checkForGuesses(_ textField: UITextField) -> Bool {
+        let inputArr = Array((textField.text ?? "Error")!)
+        for (index,value) in inputArr.enumerated(){
+            print("index: \(index) value: \(value)")
+        }
+        
+    return true
+    }
 }
 
